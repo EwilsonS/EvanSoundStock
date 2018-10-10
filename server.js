55997aaa3d82101efaf4d90df8ae6fbf845a2700
unknown <ewilsons@aol.com>
2018-10-09 22:04:54 -0400

    catch

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/api/user/:id', function(req, res, next) {
 });
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, './client/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, './client/public/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
