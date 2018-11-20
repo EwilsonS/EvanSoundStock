01934dee15e3df83318d5bd2f8b8ed9a88e041bf
unknown <ewilsons@aol.com>
2018-11-19 22:29:23 -0500

    nav polish login loginc begin

--- client/src/components/Navbar/Nav.js
+++ client/src/components/Navbar/Nav.js
@@ -12,13 +12,21 @@ export const Nav = () => (
     <div className="col-md-3 left-space">
       <Link to="/">
         <span className="navbar-brand text-info nav-title">
-          <span className="text-info h4"><strong> <span className="flash">S</span>ound<span className="flash2">S</span>tock</strong></span>
+          <span className="text-info h4">
+            <strong>
+              {" "}
+              <span className="flash">S</span>ound
+              <span className="flash2">S</span>tock
+            </strong>
+          </span>
         </span>
       </Link>
     </div>
     <div className="col-md-2">
       <Link to="/AllArtists" className="">
-        <button className="btn btn-danger text-light btn-sm m-1 button-nav">View All Artists</button>
+        <button className="btn btn-danger text-light btn-sm m-1 button-nav">
+          View All Artists
+        </button>
       </Link>
     </div>
     <div className="col-md-7">
@@ -27,10 +35,14 @@ export const Nav = () => (
           <div className="register rounded float-right">
             <p>Register</p>
             <Link to="../InvestorSignUp">
-              <button className="btn btn-info m-3">Investor</button>
+              <div className="invest">
+                <span className="reg-btn-txt">Investor</span>
+              </div>
             </Link>
             <Link to="../ArtistSignUp">
-              <button className="btn btn-info m-3">Artist</button>
+              <div className="artist">
+                <span className="reg-btn-txt">Artist</span>
+              </div>
             </Link>
           </div>
         </div>
@@ -39,14 +51,28 @@ export const Nav = () => (
         <div className="col-md-12">
           <br />
           <br />
-          <br />
           <div className="form-group input-icons float-right">
             <form>
               <span className="">
-                <i class="fas fa-user"></i>
-                <input type="text" class="form-control-sm login-input m-1" placeholder="Email" aria-label="Recipient's username"></input>
-                <i class="fas fa-lock ml-3"></i>
-                <input type="password" class="form-control-sm login-input m-1" placeholder="Password" aria-label="Recipient's username"></input>
+                <i class="fas fa-user" />
+                <input
+                  type="text"
+                  className="form-control-sm login-input m-1"
+                  placeholder="Email"
+                />
+                <i class="fas fa-lock ml-3" />
+                <input
+                  type="password"
+                  className="form-control-sm login-input m-1"
+                  placeholder="Password"
+                />
+                <button
+                  type="submit"
+                  className="btn btn-sm login-input m-2"
+                  placeholder="Go"
+                >
+                Go
+                </button>
               </span>
             </form>
           </div>
